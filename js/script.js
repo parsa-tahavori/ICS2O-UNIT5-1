d2809c9067642c5d24a3d5270ed53f5bd3a4b6c2
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-11-16 19:09:30 -0500

    ,..,

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
+ if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Unit-5-01-JS/sw.js", {
     scope: "/ICS20-Unit-5-01-JS/",
   })
